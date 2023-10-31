ae7b5d708cfe9ff46f55037e48f9f3ca52e9f15c
Moukarnold <moukarnold@gmail.com>
2023-10-31 23:08:27 +0100

    setting of the ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/investment-calculator-logo.png';
+import Forms from './components/Forms';
+import Screening from './components/Screening';
+
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <h1>Investment Calculator</h1>
       </header>
+       
+       <Forms/>
+      
+       <Screening/>
+
+      
     </div>
   );
 }
